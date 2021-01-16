c78a9171866dde3d6a1849cab3119c6c4bcc3f53
tbanish <tombanish@gmail.com>
2021-01-16 16:51:02 -0500

    build user params and create action in users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,24 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else
+      render :new
+    end
+  end
+
   def show
     @user = User.find_by(id: session[:user_id])
   end
+end
+
+private
+
+def user_params
+  params.require(:user).permit(:username, :password)
 end
