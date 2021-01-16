7b73e32a9258972df6b9171adf71870f128b0a4f
tbanish <tombanish@gmail.com>
2021-01-16 16:03:45 -0500

    build create action in sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,4 +2,15 @@ class SessionsController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.find_by(username: params[:user][:username])
+
+    if @user && @user.authenticate(params[:user][:password])
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else
+      render :new
+    end
+  end
 end
