89c534a82b1e22c0325698d23d5c90fa3c49320c
tbanish <tombanish@gmail.com>
2021-01-19 16:03:52 -0500

    build show action in notes controller

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,2 +1,7 @@
 class NotesController < ApplicationController
+  def show
+    @routine = Routine.find_by(id: params[:routine_id])
+    @item = Item.find_by(id: params[:item_id])
+    @note = Note.find_by(id: params[:id])
+  end
 end
