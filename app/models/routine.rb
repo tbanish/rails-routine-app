e79b9940e69b83bb6d5d3a86a430febdf0e6115c
tbanish <tombanish@gmail.com>
2021-01-19 12:11:50 -0500

    build accepts nested attributes for method in routine model

--- app/models/routine.rb
+++ app/models/routine.rb
@@ -2,4 +2,5 @@ class Routine < ApplicationRecord
   belongs_to :instrument
   belongs_to :user
   has_many :items
+  accepts_nested_attributes_for :instrument, reject_if: proc {|attributes| attributes['name'].blank?}
 end
