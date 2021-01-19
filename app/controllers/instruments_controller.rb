47f4683e02a458aa6f8122edb81daadae65ba008
tbanish <tombanish@gmail.com>
2021-01-19 11:07:51 -0500

    build instrument show action and instrument_params

--- app/controllers/instruments_controller.rb
+++ app/controllers/instruments_controller.rb
@@ -2,4 +2,14 @@ class InstrumentsController < ApplicationController
   def index
     @instruments = current_user.instruments
   end
+
+  def show
+    @instrument = Instrument.find_by(id: params[:id])
+  end
+
+  private
+
+  def instrument_params
+    params.require(:instrument).permit(:name)
+  end
 end
