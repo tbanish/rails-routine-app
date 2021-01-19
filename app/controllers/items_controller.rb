13e01b7f56f013a116943db434011f89b1aee4ba
tbanish <tombanish@gmail.com>
2021-01-19 14:31:00 -0500

    build show action in items controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,4 +3,9 @@ class ItemsController < ApplicationController
     @routine = Routine.find_by(id: params[:routine_id])
     @items = @routine.items
   end
+
+  def show
+    @item = Item.find_by(id: params[:id])
+    @routine = Routine.find_by(id: params[:routine_id])
+  end
 end
