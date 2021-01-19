b74082a3f2624909571cdb348019dfb18878aeb1
tbanish <tombanish@gmail.com>
2021-01-19 16:03:21 -0500

    nest note resources under items

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,12 @@ Rails.application.routes.draw do
   post '/login' => 'sessions#create'
   delete '/logout' => 'sessions#destroy'
 
-  resources :notes
+
   resources :instruments
   resources :routines do
-    resources :items
+    resources :items do
+      resources :notes
+    end
   end
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
