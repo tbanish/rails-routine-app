2f77119581d745bf8a160c2436bfeac0d09c1cbb
tbanish <tombanish@gmail.com>
2021-01-19 10:59:04 -0500

    build helper methods current_user and logged_in?

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+
+  private
+
+  def logged_in?
+    !!current_user
+  end
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
 end
