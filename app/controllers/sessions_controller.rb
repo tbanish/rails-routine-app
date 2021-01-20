63f0f96f253c4d68ca7e3c38ef01f4733b0b77da
tbanish <tombanish@gmail.com>
2021-01-20 13:08:56 -0500

    build omniauth action and private auth mehtod in sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -17,8 +17,21 @@ class SessionsController < ApplicationController
     end
   end
 
+  def omniauth
+    @user = User.create_user_with_google(auth)
+
+    session[:user_id] = @user.id
+    redirect_to user_path(@user)
+  end
+
   def destroy
     session.clear
     redirect_to login_path
+  end
+
+  private
+
+  def auth
+    request.env['omniauth.auth']
   end
 end
