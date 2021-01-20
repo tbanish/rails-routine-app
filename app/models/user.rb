d7cffeb1c433c83ad977ddbc6bda8daa47491504
tbanish <tombanish@gmail.com>
2021-01-20 13:09:35 -0500

    build create_user_with_google class method in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,11 @@ class User < ApplicationRecord
   has_many :routines
   has_many :instruments, through: :routines
   has_secure_password
+
+  def self.create_user_with_google(auth)
+    self.find_or_create_by(username: auth[:info][:email]) do |u|
+      u.password = SecureRandom.hex
+    end
+  end
+
 end
