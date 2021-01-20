2306ba12b3e5266bdb4d71c1231b71f231f4a539
tbanish <tombanish@gmail.com>
2021-01-20 12:30:30 -0500

    add route for omniauth callback

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   delete '/logout' => 'sessions#destroy'
+  get '/auth/google_oauth2/callback' => 'sessions#omniauth'
 
 
   resources :instruments
