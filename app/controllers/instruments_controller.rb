907941bed32998d9a664da24ae811d2be1ca495b
tbanish <tombanish@gmail.com>
2021-01-21 16:53:56 -0500

    add before action require_login for index and show, and add logic to show action to prevent unauthorized access to instruments show page

--- app/controllers/instruments_controller.rb
+++ app/controllers/instruments_controller.rb
@@ -1,10 +1,16 @@
 class InstrumentsController < ApplicationController
+  before_action :require_login, only: [:index, :show]
+
   def index
     @instruments = current_user.instruments
   end
 
   def show
     @instrument = Instrument.find_by(id: params[:id])
+
+    if !current_user.instruments.include?(@instrument)
+      redirect_to instruments_path
+    end
   end
 
   private
