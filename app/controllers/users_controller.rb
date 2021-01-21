d5d07e54eae632480d24c3fc50186cebebb6cf73
tbanish <tombanish@gmail.com>
2021-01-21 15:52:40 -0500

    add logic to show action to prevent unauthorized access to user show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,6 +16,12 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find_by(id: session[:user_id])
+
+    if @user && @user == current_user
+      render :show
+    else
+      redirect_to login_path
+    end
   end
 end
 
