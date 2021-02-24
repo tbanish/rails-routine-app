cac1f221217ebe8babc44a8f8d1b31823cdf50bd
tbanish <tombanish@gmail.com>
2021-02-24 16:42:28 -0500

    create scope method :most_routines

--- app/models/instrument.rb
+++ app/models/instrument.rb
@@ -1,4 +1,5 @@
 class Instrument < ApplicationRecord
   has_many :routines
   has_many :users, through: :routines
+  scope :most_routines, -> {joins(:routines).group(:id).order('count(instruments.id) desc')}
 end
