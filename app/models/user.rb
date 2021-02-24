482cd6a7d3cedbf4790368ef367426ba4843aea2
tbanish <tombanish@gmail.com>
2021-02-24 15:17:35 -0500

    validate username, uniqueness true

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_many :routines
   has_many :instruments, through: :routines
-  validates :username, presence: true
+  validates :username, presence: true, uniqueness: true
   has_secure_password
 
   def self.create_user_with_google(auth)
