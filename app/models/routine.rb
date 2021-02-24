a7717349169f1492e58c4082cef5a6617fbd5cec
tbanish <tombanish@gmail.com>
2021-02-24 14:36:40 -0500

    validate routine, name and instrument_id must be present

--- app/models/routine.rb
+++ app/models/routine.rb
@@ -2,5 +2,6 @@ class Routine < ApplicationRecord
   belongs_to :instrument
   belongs_to :user
   has_many :items
+  validates :name, :instrument_id, presence: true
   accepts_nested_attributes_for :instrument, reject_if: proc {|attributes| attributes['name'].blank?}
 end
