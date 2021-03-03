e9aae4fc33d2360f713f38aa07392ae092a37afd
tbanish <tombanish@gmail.com>
2021-03-03 14:47:41 -0500

    validate description and goal, length max 150

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,6 @@
 class Item < ApplicationRecord
   belongs_to :routine
   has_many :notes
-  validates :name, :description, :goal, presence: true
+  validates :name, presence: true
+  validates :description, :goal, presence: true, length: { maximum: 150 }
 end
