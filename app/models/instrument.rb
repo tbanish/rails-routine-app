7280fceaee98e99671e7ce99ee805598d3203639
tbanish <tombanish@gmail.com>
2021-03-03 14:36:00 -0500

    validate instrument name, uniqueness case_sensitive: false

--- app/models/instrument.rb
+++ app/models/instrument.rb
@@ -2,5 +2,5 @@ class Instrument < ApplicationRecord
   has_many :routines
   has_many :users, through: :routines
   scope :most_routines, -> {joins(:routines).group(:id).order('count(instruments.id) desc')}
-  validates :name, uniqueness: true, presence: true
+  validates :name, uniqueness: { case_sensitive: false }, presence: true
 end
