af3176ab275a21a6f7af64bafad6c9516c31ac5a
tbanish <tombanish@gmail.com>
2021-03-12 14:08:08 -0500

    prevent acces to note show page if item.notes does not include note

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -5,7 +5,7 @@ class NotesController < ApplicationController
   before_action :set_note, only: [:show, :edit, :update, :destroy]
 
   def show
-    if !@note || @note.item.routine.user != current_user || @routine == nil || current_user != @routine.user
+    if !@note || @note.item.routine.user != current_user || !@item.notes.include?(@note) || @routine == nil || current_user != @routine.user
       redirect_to user_path(current_user)
     end
   end
