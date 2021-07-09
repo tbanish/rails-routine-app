f4145591577d3fdf7a6dc792068ca5b3587ccb93
tbanish <tombanish@gmail.com>
2021-07-09 15:54:33 -0400

    migrate tips table to database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_15_194322) do
+ActiveRecord::Schema.define(version: 2021_07_09_195231) do
 
   create_table "instruments", force: :cascade do |t|
     t.string "name"
@@ -44,6 +44,13 @@ ActiveRecord::Schema.define(version: 2021_01_15_194322) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["instrument_id"], name: "index_routines_on_instrument_id"
     t.index ["user_id"], name: "index_routines_on_user_id"
+  end
+
+  create_table "tips", force: :cascade do |t|
+    t.string "title"
+    t.string "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
