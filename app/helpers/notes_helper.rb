82363a5bb6d4e6d6abbe6d4955d59ca82a03ca52
tbanish <tombanish@gmail.com>
2021-07-13 00:13:36 -0400

    build truncate method to truncate note content

--- app/helpers/notes_helper.rb
+++ app/helpers/notes_helper.rb
@@ -2,4 +2,22 @@ module NotesHelper
   def date(note)
     note.created_at.localtime.strftime("%B %d, %Y")
   end
+
+  def truncate(note)
+    noteArray = note.content.split(" ")
+
+    if noteArray.length < 8
+      return note.content
+    end
+    
+    array = []
+    counter = 0
+
+    while counter < 8 do
+      array.push(noteArray[counter])
+      counter += 1
+    end
+
+    array.join(" ") + " ..."
+  end
 end
